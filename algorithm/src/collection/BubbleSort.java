36b3b1f874c3b816a0485e9ec7047d40968d014e
elqqmffntl1 <elqqmffntl@gmail.com>
2016-08-05 17:25:57 +0900

    알고리즘..기능사

---
+++ algorithm/src/collection/BubbleSort.java
@@ -0,0 +1,28 @@
+package collection;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+     public static void main(String[] args) {
+           new BubbleSort().execute();
+     }
+     int i,j,temp,length=5;
+     int datas[] = new int[length];
+     public void execute(){
+          Scanner scanner = new Scanner(System.in);
+          System. out.println("TOTAL COUNT is 5 " );
+           for(i =0;i <length ;i ++){} //문제
+          System. out.println();
+           for(i =0;i <length ;i ++){} //문제
+     }
+     public void print(){
+          System. out.println("DATA COUNT : " +length );
+          String result = "";
+           for(i =0;i <datas .length ;i ++){
+               result += datas[ i]+ " , ";
+          }
+          System. out.println("DATA in ascending order \n"
+          + result.substring(0, result.length()-2));
+
+     }
+}
